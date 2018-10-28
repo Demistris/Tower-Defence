81378be958baba7660cee6bf92d1aa1faf9aa6fa
Kaja Więckowska <k.wieckowska66@gmail.com>
2018-10-28 17:43:50 +0100

    Added all the files

---
+++ Assets/Scripts/Buy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buy : MonoBehaviour {
+
+    public GameObject standardTower;
+    public Vector3 positionOffset;
+    public Spot targetSpot;
+    BuildManager buildManager;
+
+	void Start ()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void onClick()
+    {
+        GameObject towerToBuild = BuildManager.instance.GetTowerToBuild();
+        standardTower = (GameObject)Instantiate(towerToBuild, targetSpot.transform.position + positionOffset, targetSpot.transform.rotation);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Spot.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 positionOffset;
+    public Vector3 buttonPosition;
+
+    public GameObject buyButton;
+    public GameObject canvas;
+    public GameObject tower;
+
+    private Renderer rend;
+    private Color startColor;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
+    }
+
+    void OnMouseDown()
+    {
+        buyButton.GetComponent<Buy>().targetSpot = this;
+
+        if (tower != null)
+        {
+            Debug.Log("Can't build in here!");
+            return;
+        }
+
+        Instantiate(buyButton, transform.position + buttonPosition, Quaternion.Euler(75, 0, 0), canvas.transform);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
