d93a2daa3e29f2973135e510590f8e0a91f5c6d9
Kaja Więckowska <k.wieckowska66@gmail.com>
2019-02-19 17:31:32 +0100

    Music toggle works

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
     public string levelToLoad = "Level01";
     public SceneFader sceneFader;
+
+    private const string IS_MUSIC_ON = "IsMusicOn";
+    public AudioMixerGroup musicGroup;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt(IS_MUSIC_ON) == 1)
+        {
+            musicGroup.audioMixer.SetFloat("musicVolume", 0f);
+        }
+        else
+        {
+            musicGroup.audioMixer.SetFloat("musicVolume", -80f);
+        }
+    }
 
     public void Play()
     {
---
+++ Assets/Scripts/MusicButton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicButton : MonoBehaviour
+{
+    private const string IS_MUSIC_ON = "IsMusicOn";
+    public AudioMixerGroup musicGroup;
+    public Toggle toggle;
+    private bool isStartOver;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt(IS_MUSIC_ON) == 1)
+        {
+            toggle.isOn = true;
+        }
+        else
+        {
+            toggle.isOn = false;
+        }
+
+        isStartOver = true;
+    }
+
+    public void TurnOff()
+    {
+        musicGroup.audioMixer.SetFloat("musicVolume", -80f);
+        toggle.isOn = false;
+    }
+
+    public void TurnOn()
+    {
+        musicGroup.audioMixer.SetFloat("musicVolume", 0f);
+        toggle.isOn = true;
+    }
+
+    public void IsMusicOn()
+    {
+        if (isStartOver == false)
+        {
+            return;
+        }
+
+        if (PlayerPrefs.GetInt(IS_MUSIC_ON) == 1)
+        {
+            TurnOff();
+            PlayerPrefs.SetInt(IS_MUSIC_ON, 0);
+        }
+        else
+        {
+            TurnOn();
+            PlayerPrefs.SetInt(IS_MUSIC_ON, 1);
+        }
+    }
+}
